0c4025bd6362b5db22144bf021926086249befb2
Aleson Batista <alesonbatista@hotmail.com>
2025-08-28 17:11:07 -0300

    Esqueci do commit antes do push (-_-)

--- conclave.js
+++ conclave.js
@@ -1,2 +1,14 @@
 const entrada = require('prompt-sync')();
 
+console.log("**** Conclave ****");
+
+// Cadastra os 5 cardeais
+let cardeais = [];
+for (let i = 1; i <= 5; i++) {
+    let nome = entrada(`Digite o nome do cardeal ${i}: `);
+    cardeais.push({id: i, nome: nome, votos: 0});
+}
+
+const quorum = Math.ceil((2/3) * cardeais.length); // Número mínimo para que a votação seja válida
+let eleito = null;
+let rodada = 1;
